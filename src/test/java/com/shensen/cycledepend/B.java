4d64342ac283cae74ae01e1f384c26c1320f5d2c
Alwyn <nowtry@yeah.net>
2021-03-16 11:28:51 +0800

    Learning cycle dependence.

---
+++ src/test/java/com/shensen/cycledepend/B.java
@@ -0,0 +1,20 @@
+package com.shensen.cycledepend;
+
+public class B {
+
+    private A a;
+
+    public A getA() {
+        return a;
+    }
+
+    public void setA(A a) {
+        this.a = a;
+    }
+
+    public B() {
+        System.out.println("----B created success----");
+    }
+}
+ 
+ 
